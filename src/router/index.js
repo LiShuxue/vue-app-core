0dd005d69d4e5eecee39967661af4d6679d9961b
Li Shuxue <Shuxue.Li@sc.com>
2018-05-09 13:40:46 +0800

    删除自带内容，修改缩进格式，初始化路由逻辑

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import App from '@/App.vue'
+import Hello from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+    routes: [
+        {
+            path: '/',
+            component: App,
+            children: [
+                { path: '/hello', component: Hello }
+            ]
+        }
+    ]
 })
