25aedb10d858ee04fa290bbede2a9e3cd9bd03d6
LiShuxue <1149926505@qq.com>
2018-05-09 16:06:50 +0800

    使用<keep-alive>来缓存部分页面和组件

---
+++ src/page/pageA.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="page">
+        <p>this is pageA.vue, need be storage in cache</p>
+        输入一些数据并手动跳转到别的页面，再回来，发现数据依然在
+        <input placeholder="please enter"/>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+</style>
