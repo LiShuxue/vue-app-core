a28759670fb1f290a3bc0420e9fb3bcc12a1a54f
Li Shuxue <Shuxue.Li@sc.com>
2018-05-10 18:10:39 +0800

    props的类型检查

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,7 +9,7 @@
         </computer>
         <!-- 通过props从父组件往子组件传递参数，可以传静态内容，也可以通过绑定v-bind:propertyName="..."来传递动态内容。 -->
         <!-- camelCase vs kebab-case。如果在template中，两种写法都可以。但是如果在html中，特性名是大小写不敏感的，所以浏览器会把所有大写字符解释为小写字符 -->
-        <prop-test propA="静态字符串" :propB="msg" :prop-c="msg"></prop-test>
+        <prop-test propA="静态字符串" :propB="msg" :prop-c="msg" propE="success"></prop-test>
     </div>
 </template>
 
--- src/components/propTest.vue
+++ src/components/propTest.vue
@@ -1,18 +1,43 @@
 <template>
     <div class="prop-test">
-        <!-- props 可以用来在父子组件中传参(只能从父到子)。当一个值传递给一个 prop 特性的时候，它就变成了那个组件实例的一个属性，可以在组件中直接使用或者用this.使用 -->
+        <!-- props 可以用来在父子组件中传参(单项数据流，只能从父到子)。当一个值传递给一个 prop 特性的时候，它就变成了那个组件实例的一个属性，可以在组件中直接使用或者用this.使用 -->
         <!-- props 选项需要在组件中注册声明， props: ['title', 'A', 'B']。 一个组件默认可以拥有任意数量的 prop，任何值都可以传递给任何 prop-->
         <!-- 子组件中使用：<test title="My journey with Vue" A="testA" B="testB"></test> -->
         <!-- 使用 v-bind 来动态传递 prop。如 v-bind:key="post.id" -->
+        <!-- 每次父级组件发生更新时，子组件中所有的 prop 都将会刷新为最新的值。这意味着你不应该在一个子组件内部改变 prop。如果你这样做了，Vue 会在浏览器的控制台中发出警告。但是如果你需要使用这个prop的值，最好定义一个本地的 data 属性并将这个 prop 用作其初始值 -->
         <div><p>父组件传递的数据A：{{propA}}</p></div>
         <div><p>父组件传递的数据B：{{propB}}</p></div>
         <div><p>父组件传递的数据C：{{this.propC}}</p></div>
+        <div><p>父组件传递的数据D：{{propD}}</p></div>
+        <div><p>父组件传递的数据E：{{propE}}</p></div>
     </div>
 </template>
 
 <script>
 export default {
-    props: ['propA', 'propB', 'propC']
+    props: {
+        // 基础的类型检查 (`null` 匹配任何类型)
+        propA: String,
+        // 多个可能的类型
+        propB: [String, Number],
+        // 必填的字符串
+        propC: {
+            type: String,
+            required: true
+        },
+        // 带有默认值的数字
+        propD: {
+            type: Number,
+            default: 100
+        },
+        // 自定义验证函数
+        propE: {
+            validator: function (value) {
+                // 这个值必须匹配下列字符串中的一个
+                return ['success', 'warning', 'danger'].indexOf(value) !== -1
+            }
+        }
+    }
 }
 </script>
 
