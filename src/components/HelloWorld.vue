5b49aa292cb741edcece7c5bcf42032e5f558342
Li Shuxue <Shuxue.Li@sc.com>
2018-05-10 17:26:49 +0800

    通过props从父组件往子组件传递参数

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,18 +2,29 @@
     <div class="hello">
         this is HelloWorld.vue
         <computer>
+            <!-- slot可以将不同的内容渲染到组件的不同地方，这些内容也可以是原生的html -->
             <div slot="CPU">Intel Core i7</div>
             <div slot="GPU">GTX980Ti</div>
             <div slot="Memory">Kingston 32G</div>
         </computer>
+        <!-- 通过props从父组件往子组件传递参数，可以传静态内容，也可以通过绑定v-bind:propertyName="..."来传递动态内容。 -->
+        <!-- camelCase vs kebab-case。如果在template中，两种写法都可以。但是如果在html中，特性名是大小写不敏感的，所以浏览器会把所有大写字符解释为小写字符 -->
+        <prop-test propA="静态字符串" :propB="msg" :prop-c="msg"></prop-test>
     </div>
 </template>
 
 <script>
 import computer from '@/components/computer'
+import propTest from '@/components/propTest'
 export default {
+    data () {
+        return {
+            msg: 'a'
+        }
+    },
     components: {
-        computer
+        computer,
+        propTest
     }
 }
 </script>
---
+++ src/components/propTest.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="prop-test">
+        <!-- props 可以用来在父子组件中传参(只能从父到子)。当一个值传递给一个 prop 特性的时候，它就变成了那个组件实例的一个属性，可以在组件中直接使用或者用this.使用 -->
+        <!-- props 选项需要在组件中注册声明， props: ['title', 'A', 'B']。 一个组件默认可以拥有任意数量的 prop，任何值都可以传递给任何 prop-->
+        <!-- 子组件中使用：<test title="My journey with Vue" A="testA" B="testB"></test> -->
+        <!-- 使用 v-bind 来动态传递 prop。如 v-bind:key="post.id" -->
+        <div><p>父组件传递的数据A：{{propA}}</p></div>
+        <div><p>父组件传递的数据B：{{propB}}</p></div>
+        <div><p>父组件传递的数据C：{{this.propC}}</p></div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['propA', 'propB', 'propC']
+}
+</script>
+
+<style scoped>
+</style>
