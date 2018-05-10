36140549c304fb1f741865c8a123e37ec2cb4383
LiShuxue <1149926505@qq.com>
2018-05-10 11:39:40 +0800

    通过slot插槽实现内容分发

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,20 @@
 <template>
     <div class="hello">
         this is HelloWorld.vue
+        <computer>
+            <div slot="CPU">Intel Core i7</div>
+            <div slot="GPU">GTX980Ti</div>
+            <div slot="Memory">Kingston 32G</div>
+        </computer>
     </div>
 </template>
 
 <script>
+import computer from '@/components/computer'
 export default {
+    components: {
+        computer
+    }
 }
 </script>
 
