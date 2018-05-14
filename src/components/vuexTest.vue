5bdbfe8f4cebda0553ba4b62b5e956080c7d2aa2
LiShuxue <1149926505@qq.com>
2018-05-14 15:18:44 +0800

    通过mapGetters将 store 中的 getter 映射到计算属性

--- src/components/vuexTest.vue
+++ src/components/vuexTest.vue
@@ -7,11 +7,12 @@
 
         <!-- 使用Vuex的各种map方法后 -->
         <div>将store中的state map成该组件中的计算属性: {{testState}}</div>
+        <div>将store中的getter map成该组件中的计算属性: {{testGetter}}</div>
     </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState, mapGetters } from 'vuex'
 export default {
     computed: {
         // 通过mapState可以帮助我们生成计算属性。将store中的某个state映射到我们当前组件的某个计算属性上。
@@ -19,6 +20,13 @@ export default {
         // 如果当前计算属性与state中的名字相同：...mapState([ 'testState' ])，用的是中括号[]。推荐使用这种
         ...mapState([
             'testState'
+        ]),
+
+        // mapGetters 可以将 store 中的 getter 映射到计算属性
+        // 如果当前计算属性与getter的名字不同：...mapGetters({ mapG: 'testGetter' })，用的是大括号{}
+        // 如果当前计算属性与getter的名字相同：...mapGetters([ 'testGetter' ])，用的是中括号[]。推荐使用这种
+        ...mapGetters([
+            'testGetter'
         ])
     },
     methods: {
