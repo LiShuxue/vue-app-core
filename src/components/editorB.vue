a15d0c9a01fb0374ddf8be7d1056e5b74e416f4d
LiShuxue <1149926505@qq.com>
2018-08-27 10:07:11 +0800

    使用Markdown编辑器 mavonEditor

---
+++ src/components/editorB.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="editor-b">
+        <h5>this is editor-B.vue</h5>
+        <mavon-editor class="editor-content" @change="editorContentChange" @save="editorContentSave"></mavon-editor>
+    </div>
+</template>
+<script>
+import { mavonEditor } from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
+export default {
+    components: {
+        mavonEditor
+    },
+    methods: {
+        // 第一个参数是markdown文本， 第二个参数是html文本
+        editorContentChange (value, render) {
+            // console.log(value)
+            // console.log(render)
+        },
+        // 第一个参数是markdown文本， 第二个参数是html文本
+        editorContentSave (value, render) {
+            console.log(value)
+            console.log(render)
+        }
+    }
+}
+</script>
+<style>
+.editor-b{
+    border: 1px blue solid;
+    margin: 10px 0 0 0;
+    padding: 0 10px;
+}
+.editor-content{
+    height: 600px;
+    margin-bottom: 20px;
+}
+</style>
