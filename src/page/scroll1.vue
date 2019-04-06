cd7ff9de936b2defbfa3f2c64818a4512e420aed
LiShuxue <1149926505@qq.com>
2019-04-06 18:17:51 +0800

    使用better-scroll做滚动效果

---
+++ src/page/scroll1.vue
@@ -0,0 +1,84 @@
+<template>
+    <!--
+        最简单的实现 ：
+        直接使用 new BScroll(this.$refs.wrapper, {})
+    -->
+    <div class="wrapper" ref="wrapper">
+        <div class="content">
+            <ul>
+                <li v-for="(item, index) in testList" :key="index">
+                    {{ item }} - {{ index }} - {{ item.msg }}
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+import BScroll from 'better-scroll'
+export default {
+    data () {
+        return {
+            testList: [
+                { msg: '1' },
+                { msg: '2' },
+                { msg: '3' },
+                { msg: '1' },
+                { msg: '2' },
+                { msg: '3' },
+                { msg: '1' },
+                { msg: '2' },
+                { msg: '3' },
+                { msg: '1' },
+                { msg: '2' },
+                { msg: '3' },
+                { msg: '1' },
+                { msg: '2' },
+                { msg: '3' },
+                { msg: '1' },
+                { msg: '2' },
+                { msg: '3' },
+                { msg: '1' },
+                { msg: '2' },
+                { msg: '3' },
+                { msg: '1' },
+                { msg: '2' },
+                { msg: '3' },
+                { msg: '1' },
+                { msg: '2' },
+                { msg: '3' },
+                { msg: '1' },
+                { msg: '2' },
+                { msg: '3' },
+                { msg: '1' },
+                { msg: '2' },
+                { msg: '3' },
+                { msg: '1' },
+                { msg: '2' },
+                { msg: '3' },
+                { msg: '1' },
+                { msg: '2' },
+                { msg: '3' },
+                { msg: '1' },
+                { msg: '2' },
+                { msg: '3' },
+                { msg: '1' },
+                { msg: '2' },
+                { msg: '3' }
+            ]
+        }
+    },
+    mounted () {
+        this.$nextTick(() => {
+            this.scroll = new BScroll(this.$refs.wrapper, {})
+        })
+    }
+}
+</script>
+
+<style scoped>
+.wrapper {
+    height: calc(100vh - 200px);
+    overflow: hidden;
+}
+</style>
---
+++ src/page/scroll2.vue
@@ -0,0 +1,81 @@
+<template>
+    <!--
+        将scroll功能封装成一个组件，然后使用这个组件scroll-view ：
+    -->
+    <scroll-view class="scroll-view" :data="testList">
+        <div class="content">
+            <ul>
+                <li v-for="(item, index) in testList" :key="index">
+                    {{ item }} - {{ index }} - {{ item.msg }}
+                </li>
+            </ul>
+        </div>
+    </scroll-view>
+</template>
+
+<script>
+import ScrollView from '@/components/ScrollView'
+export default {
+    data () {
+        return {
+            testList: [
+                { msg: '1' },
+                { msg: '2' },
+                { msg: '3' },
+                { msg: '1' },
+                { msg: '2' },
+                { msg: '3' },
+                { msg: '1' },
+                { msg: '2' },
+                { msg: '3' },
+                { msg: '1' },
+                { msg: '2' },
+                { msg: '3' },
+                { msg: '1' },
+                { msg: '2' },
+                { msg: '3' },
+                { msg: '1' },
+                { msg: '2' },
+                { msg: '3' },
+                { msg: '1' },
+                { msg: '2' },
+                { msg: '3' },
+                { msg: '1' },
+                { msg: '2' },
+                { msg: '3' },
+                { msg: '1' },
+                { msg: '2' },
+                { msg: '3' },
+                { msg: '1' },
+                { msg: '2' },
+                { msg: '3' },
+                { msg: '1' },
+                { msg: '2' },
+                { msg: '3' },
+                { msg: '1' },
+                { msg: '2' },
+                { msg: '3' },
+                { msg: '1' },
+                { msg: '2' },
+                { msg: '3' },
+                { msg: '1' },
+                { msg: '2' },
+                { msg: '3' },
+                { msg: '1' },
+                { msg: '2' },
+                { msg: '3' }
+            ]
+        }
+    },
+    components: {
+        ScrollView
+    }
+}
+</script>
+
+<style scoped>
+.scroll-view {
+    height: calc(100vh - 200px);
+    overflow: hidden;
+}
+</style>
